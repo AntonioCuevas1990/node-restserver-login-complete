5c0a28814f650c1b0f8f4b1cc21f9971894a0372
Antonio Cuevas <jantoniocuevas90@gmail.com>
2019-12-15 21:10:51 -0600

    Cuarta Versión - Proyecto Terminado

--- server/models/redis.js
+++ server/models/redis.js
@@ -5,15 +5,4 @@ clientRedis.on("connect", function() {
     console.log("Connected to Redis");
 });
 
-let id = 5;
-
-clientRedis.hgetall(id, function(err, obj) {
-    if (!obj) {
-        console.log("El usuario no existe");
-    } else {
-        obj.id = id;
-        console.log(obj);
-    }
-});
-
 module.exports = clientRedis;
--- server/server.js
+++ server/server.js
@@ -10,13 +10,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-//hablitar la carpeta public
-app.use(express.static(path.resolve(__dirname, "../public")));
-
 db.sequelize
     .query("SET FOREIGN_KEY_CHECKS = 0", { raw: true })
     .then(function(results) {
-        db.sequelize.sync().then(() => {
+        db.sequelize.sync({ force: true }).then(() => {
             console.log("Borrar y Resincronizar con {force: true}");
         });
     });
@@ -24,16 +21,8 @@ db.sequelize
 const userController = require("./controllers/user-controller");
 app.use(userController);
 
-//Vistas
 //Configuración global de rutas
 app.use(require("./controllers/auth-controller"));
-
-// habilitar la carpeta public
-app.use(express.static(__dirname + "/public"));
-
-// Configuración engine views
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
 
 app.listen(process.env.PORT, () => {
     console.log("Escuchando puerto: ", process.env.PORT);
